cdcfd33631afb6b2c9d2197ef4d14ee80d15008e
chiwaki <s_chiwaki@yahoo.co.jp>
2024-02-04 17:13:52 +0900

    add axiosとvue-router

---
+++ app/front/src/router.ts
@@ -0,0 +1,12 @@
+﻿import { createRouter,createWebHistory } from 'vue-router';
+import TopPage from "./TopPage.vue";
+ 
+const routes = [
+    { path: '/', name: 'TopPage', component: TopPage },
+]
+const router = createRouter({
+    history: createWebHistory(), // HTML5 History モード
+    routes,
+})
+ 
+export default router;
