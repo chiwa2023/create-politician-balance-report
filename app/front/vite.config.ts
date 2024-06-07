9685b1ec2b6ea1ade115dd1f486c0bbdc97eb4fd
Chiwaki <chiwa2023@ymail.ne.jp>
2024-06-08 08:09:53 +0900

    vite5.2 upgrade

--- app/front/vite.config.ts
+++ app/front/vite.config.ts
@@ -1,11 +1,7 @@
-/// <reference types="vitest" />
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
-    test: {
-        include: ["src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
-    },
-});
+  plugins: [vue()],
+})
