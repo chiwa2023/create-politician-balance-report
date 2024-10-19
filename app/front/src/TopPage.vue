3f106118e9e6d562d977817892ffa2278da80235
Chiwaki <chiwa2023@ymail.ne.jp>
2024-10-20 03:37:20 +0900

    fix axios削除

--- app/front/src/TopPage.vue
+++ app/front/src/TopPage.vue
@@ -1,15 +1,9 @@
 ﻿<script setup lang="ts">
 //import { ref, Ref } from 'vue';
-//import axios from 'axios';
 //import SelectOptionDto from './dto/selectOptionDto';
 
 //const options:Ref<SelectOptionDto[]> = ref([]);
 //async function getOptions(){
-//    alert("押す");
-//    options.value = await axios.get('http://localhost:8080/trial-api-rest')
-//    .then((response) => {return response.data})
-//    .catch((error) => {alert(error)});
-//} 
 </script>
 <template>
     <h1>(仮)Top Page</h1>
